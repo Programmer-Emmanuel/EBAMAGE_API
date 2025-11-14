73eff38db68b3d9586c1a3091817ad83f65270a7
Emmanuel Bamidélé <165570846+Programmer-Emmanuel@users.noreply.github.com>
2025-11-14 10:38:03 +0000

    render yaml

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ composer install --no-dev --optimize-autoloader
 mkdir -p storage/app/firebase
 
 # On recrée le json ici ➜ Render l'aura même si GitHub ne l'a pas
-echo "$FIREBASE_CREDENTIALS" | base64 -d > storage/app/firebase/ebamagenotificationsboutiques-firebase-adminsdk-fbsvc-96a227941d.json
+echo "$FIREBASE_SERVICE_ACCOUNT_BASE64" | base64 -d > storage/app/firebase/ebamagenotificationsboutiques-firebase-adminsdk-fbsvc-96a227941d.json
 
 php artisan config:cache
 php artisan route:cache
